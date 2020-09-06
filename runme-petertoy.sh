a806e0bce040de67f9f8a95fedc0118c26e2bb93
PeterAJansen <pajansen@email.arizona.edu>
2020-09-06 16:43:17 -0700

    additions

---
+++ runme-petertoy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#BERT_DIR="/tmp/bert/uncased_L-12_H-768_A-12"  && \
+#BERT_DIR="/home/peter/github/tensorflow/ranking/uncased_L-4_H-256_A-4_TF2"  && \
+BERT_DIR="/home/peter/github/tensorflow/ranking/uncased_L-12_H-768_A-12_TF2"  && \
+OUTPUT_DIR="/tmp/tfr/model-petertoy-bertbase/" && \
+DATA_DIR="/home/peter/github/peter-ranking/ranking" && \
+rm -rf "${OUTPUT_DIR}" && \
+bazel build -c opt \
+tensorflow_ranking/extension/examples:tfrbert_example_py_binary && \
+./bazel-bin/tensorflow_ranking/extension/examples/tfrbert_example_py_binary \
+   --train_input_pattern=${DATA_DIR}/train.toy.elwc.tfrecord \
+   --eval_input_pattern=${DATA_DIR}/eval.toy.elwc.tfrecord \
+   --bert_config_file=${BERT_DIR}/bert_config.json \
+   --bert_init_ckpt=${BERT_DIR}/bert_model.ckpt \
+   --bert_max_seq_length=128 \
+   --model_dir="${OUTPUT_DIR}" \
+   --list_size=15 \
+   --loss=softmax_loss \
+   --train_batch_size=8 \
+   --eval_batch_size=8 \
+   --learning_rate=1e-5 \
+   --num_train_steps=5000 \
+   --num_eval_steps=10 \
+   --checkpoint_secs=500 \
+   --num_checkpoints=2
---
+++ runserver-petertoy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#export MODEL_DIR=/tmp/output/servetest-model/export/latest_model/
+export MODEL_DIR=/tmp/tfr/model-petertoy/export/latest_model/
+#export MODEL_DIR=/home/peter/github/tensorflow/tensorflow_ranking_model/1599246704/
+#export MODEL_DIR=/home/peter/github/tensorflow/tensorflow_ranking_model
+#export MODEL_DIR=/home/peter/github/tensorflow/
+
+#nohup tensorflow_model_server \
+#  --rest_api_port=8501 \
+#  --model_name=testmodel \
+#  --model_base_path="${MODEL_DIR}" >server.log 2>&1
+
+tensorflow_model_server \
+  --rest_api_port=8501 \
+  --model_name=tfrbert \
+  --model_base_path="${MODEL_DIR}"
+
+#tensorflow_model_server \
+#  --rest_api_port=8501 \
+#  --model_name=tfrbert \  
+#  --model_base_path="${MODEL_DIR}"  
+  
