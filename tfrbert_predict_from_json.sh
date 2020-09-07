f46863eabd614f6026d31f8a6df0dcfd614be66b
PeterAJansen <pajansen@email.arizona.edu>
2020-09-07 16:10:39 -0700

    rename

---
+++ tfrbert_predict_from_json.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BERT_DIR="/home/peter/github/tensorflow/ranking/uncased_L-12_H-768_A-12_TF2"  && \
+python tensorflow_ranking/extension/examples/tfrbert_client_predict_from_json.py \
+    --vocab_file=${BERT_DIR}/vocab.txt \
+    --sequence_length=128 \
+    --input_file=jsonInExample-eval.json \
+    --output_file=eval.scoresOut.json \
+    --do_lower_case 
