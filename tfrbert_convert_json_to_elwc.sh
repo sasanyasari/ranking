2af6f86885714585eb0a7dc7f164134e043f8ef6
PeterAJansen <pajansen@email.arizona.edu>
2020-09-07 16:24:42 -0700

    minor changes

--- tfrbert_convert_json_to_elwc.sh
+++ tfrbert_convert_json_to_elwc.sh
@@ -3,6 +3,6 @@ BERT_DIR="/home/peter/github/tensorflow/ranking/uncased_L-12_H-768_A-12_TF2"  &&
 python tensorflow_ranking/extension/examples/tfrbert_convert_json_to_elwc.py \
     --vocab_file=${BERT_DIR}/vocab.txt \
     --sequence_length=128 \
-    --input_file=/home/peter/github/peter-ranking/ranking/rankingExample-eval.json \
+    --input_file=/home/peter/github/peter-ranking/ranking/TFRBertExample-eval.json \
     --output_file=eval.toy.elwc.tfrecord \
     --do_lower_case 
--- tfrbert_predict_from_json.sh
+++ tfrbert_predict_from_json.sh
@@ -3,14 +3,14 @@ BERT_DIR="/home/peter/github/tensorflow/ranking/uncased_L-12_H-768_A-12_TF2"  &&
 python tensorflow_ranking/extension/examples/tfrbert_client_predict_from_json.py \
     --vocab_file=${BERT_DIR}/vocab.txt \
     --sequence_length=128 \
-    --input_file=rankingExample-train.json \
+    --input_file=TFRBertExample-train.json \
     --output_file=train.scoresOut.json \
     --do_lower_case 
 
 python tensorflow_ranking/extension/examples/tfrbert_client_predict_from_json.py \
     --vocab_file=${BERT_DIR}/vocab.txt \
     --sequence_length=128 \
-    --input_file=rankingExample-eval.json \
+    --input_file=TFRBertExample-eval.json \
     --output_file=eval.scoresOut.json \
     --do_lower_case 
     
