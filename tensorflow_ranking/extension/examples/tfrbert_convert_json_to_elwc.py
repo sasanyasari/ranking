b92f6dce05bd06724755b23f1e977211cb88850a
PeterAJansen <pajansen@email.arizona.edu>
2020-09-07 13:09:22 -0700

    minor changes

--- tensorflow_ranking/extension/examples/tfrbert_convert_json_to_elwc.py
+++ tensorflow_ranking/extension/examples/tfrbert_convert_json_to_elwc.py
@@ -1,14 +1,4 @@
-# tfrbert_client_example.py
-
-from absl import flags
-import tensorflow as tf
-import tensorflow_ranking as tfr
-import grpc
-from tensorflow_serving.apis import predict_pb2
-from tensorflow_serving.apis import prediction_service_pb2_grpc
-from tensorflow_serving.apis import input_pb2
-from google.protobuf import text_format
-from google.protobuf.json_format import MessageToDict
+# tfrbert_convert_json_to_elwc.py
 
 from tensorflow_ranking.extension import tfrbert
 import json
@@ -21,7 +11,7 @@ import argparse
 #
 
 def main():
-
+    # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--vocab_file", type=str, required=True, help="/path/to/bert_model/vocab.txt")
     parser.add_argument("--sequence_length", type=int, required=True, help="typically 128, 256, 512")    
@@ -31,17 +21,6 @@ def main():
     parser.add_argument("--do_lower_case", action="store_true", help="Set for uncased models, otherwise do not include")
 
     args = parser.parse_args()
-
-    print(args)
-    #exit(1)
-
-    # Parameters (model)
-    #do_lower_case = True
-    #sequence_length = 128
-
-    # Parameters (in/out)
-    #filenameJsonIn = "/home/peter/github/peter-ranking/ranking/jsonInExample-eval.json"
-    #filenameELWCOut = "eval.toy.elwc.tfrecord"
 
     # Create helpers
     bert_helper = tfrbert_client_json.create_tfrbert_util_with_vocab(args.sequence_length, args.vocab_file, args.do_lower_case)
@@ -59,8 +38,7 @@ def main():
     print("Input file:  " + args.input_file)
     print("Output file: " + args.output_file)
 
-
-    # Example of converting from JSON to ELWC
+    # Perform conversion of ranking problemsJSON to ELWC
     bert_helper_json.convert_json_to_elwc_export(args.input_file, args.output_file)
 
     print("Success.")
