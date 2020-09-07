611a76b755266e1f3f80937f63b7d454ef150ada
PeterAJansen <pajansen@email.arizona.edu>
2020-09-07 13:06:26 -0700

    Added convert_json_elwc.sh

---
+++ convert_json_elwc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BERT_DIR="/home/peter/github/tensorflow/ranking/uncased_L-12_H-768_A-12_TF2"  && \
+python tensorflow_ranking/extension/examples/tfrbert_json_to_elwc.py \
+    --vocab_file=${BERT_DIR}/vocab.txt \
+    --sequence_length=128 \
+    --input_file=/home/peter/github/peter-ranking/ranking/jsonInExample-eval.json \
+    --output_file=eval.toy.elwc.tfrecord \
+    --do_lower_case 
